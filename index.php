37a59edfa731b82703b51c7e3315b808f43f2cfd
Ksardaslord <62423698+Ksardaslord@users.noreply.github.com>
2021-03-30 22:52:03 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,77 @@
 <?php
 
-/*
-PHP - это серверный скриптовый язык программирования с открытым исходным кодом. PHP специально сконструирован для веб-разработок с возможностью внедрения кода в HTML.
-Примеры использования PHP:
--Обработка форм
--Автоматическая генерация html стр.
+$name = "William"; // string
+echo gettype($name) . "<br>";
+echo var_dump($name) . "<br>";
+if (is_string($name))echo "$name - это строка<br>";
 
-Переменная - это временное хранилище, контейнер, в котором можно хранить какое-либо значение.
-Регистрозависимы $name и $Name - разные переменные
-*/
+$num = 33;   //integer целое число в 10 системе  +- 9 квинтилионов
+$num1 = 0b1010; // запись числа в двоичной системе (число 10)
+$num2 = 012; // запись числа в восьмиричной системе (число 10)
+$num3 = 0xA; // запись числа в шестнадцатиричной системе (число 10)
+is_int($num); //проверяет является ли число целым (возвращает 1 или )
 
-$userName; //Camel case
-$user_name; //Snake case
+$b = 3.5;  //float
+$c = 5E2;  //float равное 5 * 10^2   от 1.7E-308 до 1.7E+308
+$d = 5E-2;  //float равное 5 / 10^2 = 0.05
+if (is_float($c))echo "3.5 - число с плавающей точкой<br>";
 
-$name = "Jack";
-$century = 20;
-echo $name;
-echo 'Hello world! <br>';  
-echo 'Hello $name <br>';  
-echo "Hello $name <br>";
-echo 'Сумма чисел: 3 + 2 = '. 3+2 . "<br>";
-echo "$name was born in the {$century}th century<br>";//Явно указываем на перменную
-$century=30;
-echo "name - ", $name, "century - ", $century,"<br>";
+// Булев тип два значения ложь или истина
+$rain = true;  
+$snow = false;
 
-                                        
-//Иногда возникает необходиомость в строке выводить кавычки
-echo "company 'Blizzard Entertainment'<br>";
-echo 'company "Blizzard Entertainment"<br>';
-echo "company \"Blizzard Entertainment\"<br>"; 
+if (is_bool($rain))echo "Булев тип <br>";
+
+$var = null;  
 
 
+
+//СОСТАВНЫЕ ТИПЫ ДАННЫХ
+//Массив — это поименованная совокупность однотипных элементов, упорядоченных по индексам, определяющим положение элемента в массиве.
+$arr_number = [1,3,54,55]; // Ключ 0 знчение - 1 итд
+$arr_user = ['name' => 'William','age'=>33];  // Ассоциативный массив
+echo $arr_number[1],"<br>";   //Вывод элемента с ключом 1
+echo $arr_user['name'],"<br>";
+
+is_array($arr_user);
+print_r($arr_user);
+
+
+
+//Resource - это переменная содержащая ссылку на какой-либо внешний ресурс
+$resource = fopen('C:\OpenServer2\domains\home.home\123.txt', 'r');
+is_resource($resource);
+//Object
+$mysql = new Mysqli('localhost','root','root','darkproger');
+echo gettype($mysql);
+//print_r($mysql);
+
+
+
+//Неявное приведение типа
+$a = 5;
+echo gettype($a). "<br>";
+$a = $a*1.5;  // приведение типа к float
+echo gettype($a). "<br>";
+$a = 5;
+$b=2.24;
+echo "строка ",$a.$b;  // приводит к строке
+echo "3.14fas"*2 . "<br>";  // строку превратит в число с плав точкой (слева направо)
+
+
+//Принудитель
+$b = 3;
+$b = (string) $b;
+echo gettype($b). "<br>";
+$b = (float) $b;
+echo gettype($b). "<br>";
+$b = (bool) $b;  // 0, пустая строка,пустой массив вернут false(0)
+echo gettype($b). "<br>";
+echo $b . "<br>";
+
+$c = "33";
+$c = (int)$c;
+echo gettype($c). "<br>";
+$c = (array)$c;
+echo gettype($c). "<br>";
 ?>
-<div>
-    <span>Текущая дата: <?=date('d.m.Y H:i:s')?> г.</span>   /
-</div>
-
-
-<?php
-//КОНСТАНТА - хранит значение, но в отличии от переменной это значение может быть заданно только 1 раз
-define ("DB_LOGIN", "root"); 
-echo DB_LOGIN . "<br>";
-//МАССИВ - это упорядоченные наборы данных, представляющие собой список однотипных элементов.
-define ('COLORS',['red','green','blue']); // c php 7
-echo COLORS[0] . "<br>";
-echo defined('COLORS') . "<br>";  // Проверка существует ли константа 1 или 0
-
-
-// Встроенные константы
-echo __DIR__,"<br>";
-echo __FILE__,"<br>";
-echo __LINE__,"<br>";
-echo PHP_VERSION,"<br>";
-error_reporting(E_ALL);
